2f5e5e6c12c793c556ba5b9bcbed268934b7c0de
sandip.patil <sandip.aero@gmail.com>
2016-07-20 12:40:14 +0100

    Hashmap collection

---
+++ coreJava/src/coreJava/Employee.java
@@ -0,0 +1,63 @@
+package coreJava;
+
+public class Employee {
+
+	private int employeeId;
+	private String employeeName;
+
+	/**
+	 * @return the employeeId
+	 */
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	/**
+	 * @param employeeId the employeeId to set
+	 */
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + employeeId;
+		result = prime * result + ((employeeName == null) ? 0 : employeeName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (employeeId != other.employeeId)
+			return false;
+		if (employeeName == null) {
+			if (other.employeeName != null)
+				return false;
+		} else if (!employeeName.equals(other.employeeName))
+			return false;
+		return true;
+	}
+
+	/**
+	 * @return the employeeName
+	 */
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	/**
+	 * @param employeeName the employeeName to set
+	 */
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+}
---
+++ coreJava/src/coreJava/Tutorial.java
@@ -0,0 +1,39 @@
+package coreJava;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class Tutorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ HashMap<Employee, Integer> k = new HashMap<Employee, Integer>();
+ 
+ Employee objEmployee = new Employee();
+ objEmployee.setEmployeeId(5);
+ objEmployee.setEmployeeName("sandip");
+ 
+ Employee objEmployee1 = new Employee();
+ objEmployee1.setEmployeeId(5);
+ objEmployee1.setEmployeeName("sandip");
+ 
+ k.put(objEmployee, 3);
+ k.put(objEmployee1, 3);
+ 
+ Set<Entry<Employee, Integer>> o = k.entrySet();
+ 
+Iterator<Entry<Employee, Integer>>  i = o.iterator();
+while(i.hasNext()) {
+    Entry<Employee, Integer> me = i.next();
+    
+    Employee e = (Employee)me.getKey();
+    System.out.print(e.getEmployeeId() +":"+ e.getEmployeeName() + ": ");
+    System.out.println(me.getValue());
+ }
+	}
+
+}
+
+
