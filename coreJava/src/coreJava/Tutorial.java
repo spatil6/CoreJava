60b4d4df9e7c6cd383c2df22293b3191d2296739
sandip.patil <sandip.aero@gmail.com>
2016-07-21 12:23:17 +0100

    Refactor

--- coreJava/src/coreJava/Tutorial.java
+++ coreJava/src/coreJava/Tutorial.java
@@ -14,66 +14,68 @@ public class Tutorial {
 
 	public static void main(String[] args) {
 		
+		
+		 Employee objEmployee = new Employee();
+		 objEmployee.setEmployeeId(5);
+		 objEmployee.setEmployeeName("sandip");
+		 
+		 Employee objEmployee1 = new Employee();
+		 objEmployee1.setEmployeeId(15);
+		 objEmployee1.setEmployeeName("Aamit");
+		 
+		 Employee objEmployee2 = new Employee();
+		 objEmployee2.setEmployeeId(15);
+		 objEmployee2.setEmployeeName("saodip");
+ 
+		// Create Hashset collection
 		HashSet<Employee> _eSet = new HashSet<Employee>();
+			
+		 // Add in set collection
+		 _eSet.add(objEmployee);
+		 _eSet.add(objEmployee1);
+		 _eSet.add(objEmployee2);
+ 
+		 //Convert Set to List
+		 List<Employee> _lst = new ArrayList<Employee>(_eSet);
+		 // Sort by employyee name with custom compare class
+		 Collections.sort(_lst,new CustomComparator());
+		 //Sort by employee name with annonymous function
+		 Collections.sort(_lst,new Comparator<Employee>() {
+			 			@Override
+		 				public int compare(Employee o1, Employee o2) {
+			 				return o1.getEmployeeName().compareTo(o2.getEmployeeName());
+			 				}
+		 				});
+ 
+		 //Print list and set
+		 for (Employee employee : _eSet) {
+			System.out.println(employee.getEmployeeName());
+		 }
+		 
+		 for(Employee er : _lst)
+		 {
+			 System.out.println(er.getEmployeeName());
+		 }
+ 
+		// Create HashMap collection
+		HashMap<Employee, Integer> _eMap = new HashMap<Employee, Integer>();
+			
+		// Add in Hashmap collection
+		_eMap.put(objEmployee, 3);
+		_eMap.put(objEmployee1, 3);
 		
-		
- HashMap<Employee, Integer> k = new HashMap<Employee, Integer>();
- 
- Employee objEmployee = new Employee();
- objEmployee.setEmployeeId(5);
- objEmployee.setEmployeeName("sandip");
- 
- Employee objEmployee1 = new Employee();
- objEmployee1.setEmployeeId(15);
- objEmployee1.setEmployeeName("Aamit");
- 
- Employee objEmployee2 = new Employee();
- objEmployee2.setEmployeeId(15);
- objEmployee2.setEmployeeName("saodip");
- 
- 
- k.put(objEmployee, 3);
- k.put(objEmployee1, 3);
- 
- _eSet.add(objEmployee);
- _eSet.add(objEmployee1);
- _eSet.add(objEmployee2);
- 
- List<Employee> _lst = new ArrayList<Employee>(_eSet);
- Collections.sort(_lst,new CustomComparator());
- 
- Collections.sort(_lst,new Comparator<Employee>() {
-
-	@Override
-	public int compare(Employee o1, Employee o2) {
-		// TODO Auto-generated method stub
-		return o1.getEmployeeName().compareTo(o2.getEmployeeName());
-	}
-	 
- });
- 
- 
-for (Employee employee : _eSet) {
-	System.out.println(employee.getEmployeeName());
-}
- 
- for(Employee er : _lst)
- {
-	 System.out.println(er.getEmployeeName());
- }
- 
- 
- 
- Set<Entry<Employee, Integer>> o = k.entrySet();
- 
-Iterator<Entry<Employee, Integer>>  i = o.iterator();
-while(i.hasNext()) {
-    Entry<Employee, Integer> me = i.next();
-    
-    Employee e = (Employee)me.getKey();
-    System.out.print(e.getEmployeeId() +":"+ e.getEmployeeName() + ": ");
-    System.out.println(me.getValue());
- }
+        // Convert Hashmap to set for Iteration
+		 Set<Entry<Employee, Integer>> o = _eMap.entrySet();
+		 Iterator<Entry<Employee, Integer>>  i = o.iterator();
+		 
+		 //Print HashMap Data
+			while(i.hasNext()) {
+			    Entry<Employee, Integer> me = i.next();
+			    
+			    Employee e = (Employee)me.getKey();
+			    System.out.print(e.getEmployeeId() +":"+ e.getEmployeeName() + ": ");
+			    System.out.println(me.getValue());
+			 }
 	}
 
 }
